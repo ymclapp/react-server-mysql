24cc00f0c6e3fe36b303c6754b2fb7d4cccb84d7
ymcla <ymclapp@yahoo.com>
2022-02-10 14:37:01 -0600

    set up initial server and added files from the other server

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+var corsOptions = {
+    origin: "http://localhost:3000"
+}
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//const pg = require('pg');
+const db = require('./app/models');
+
+
+//drop existing tables and re-sync database during development
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+//   });
+
+//routes
+app.get('/', (request, response) => {
+    response.send('Home Page!');
+});
+
+app.get('*'), (request, response) =>
+    response.status(404).send('This route does not exist');
+
+require('./app/routes/product.routes')(app);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}`));
+
+
+
